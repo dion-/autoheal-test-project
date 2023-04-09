7946d6baf0869a0a97b369ea71913709b9511315
Dion Yiw <dxyiw00@gmail.com>
2023-04-09 20:42:34 +1200

    Fix

--- src/Order.test.ts
+++ src/Order.test.ts
@@ -1,14 +1,5 @@
 import { test, expect } from "bun:test";
 import { Order, Product, LineItem } from "./Order.js";
-import fs from "fs";
-
-const originalConsoleLog = console.log;
-const logFile = "console_logs.txt";
-const logStream = fs.createWriteStream(logFile, { flags: "a" });
-console.log = function (...args) {
-  logStream.write(args.join(" ") + "\n");
-  originalConsoleLog.apply(console, args);
-};
 
 const testProduct = new Product(1, "Product 1", 10);
 const testLineItem = new LineItem(1, testProduct, 1);
